4b15082366340549053272a31e4b97508f4fa5e1
wonderxue <1522038223@qq.com>
2021-09-04 20:12:10 +0800

    update

---
+++ c/shared_add/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "common.h"
+
+int main()
+{
+    printf("%d\n",add(3,1,2,3));
+    return 0;
+}
---
+++ c/shared_add/shared_add.c
@@ -0,0 +1,13 @@
+#include "common.h"
+#include <stdarg.h>
+#include <stdio.h>
+int add(int num,...)
+{
+    va_list valist;
+    int sum=0;
+    va_start(valist,num);
+    for(int i=0;i<num;i++)
+    sum+=va_arg(valist,int);
+    printf("shared lib\n");
+    return sum;
+}
